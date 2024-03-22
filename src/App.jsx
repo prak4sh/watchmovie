d4f547dd289551680573fe860e15f2fbc33dbc45
prakash <prakashtech250@gmail.com>
2024-03-22 13:46:32 +0545

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Analytics } from "@vercel/analytics/react"
 function App() {
   return (
     <>
+      <Analytics/>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home/>}/>
